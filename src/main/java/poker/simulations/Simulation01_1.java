6999704d0ae891a8d28903c20853174630aa1c34
Mauricio Coria <coriamauricio@gmail.com>
2022-12-17 15:19:42 -0300

    Adding Simulation01_0

---
+++ src/main/java/poker/simulations/Simulation01_1.java
@@ -0,0 +1,89 @@
+package poker.simulations;
+
+import poker.*;
+import poker.repartirstrategies.RepartirStrategy;
+
+import java.util.List;
+
+/*
+ * Player1 = 	6.96%
+ * Player2 = 	93.04%
+ * Tie = 		0.00%
+ *
+ * Si converge
+ */
+
+public class Simulation01_1 extends Simulator {
+    private static final int SIMULATIONS = 100000;
+    private static final int PLAYERS = 2;
+
+    public static void main(String[] args) {
+        new Simulation01_1().simulate();
+    }
+
+    @Override
+    protected int getNumberOfPlayers() {
+        return PLAYERS;
+    }
+
+    @Override
+    protected int getNumberOfSimulations() {
+        return SIMULATIONS;
+    }
+
+    private Player player1;
+    private Player player2;
+
+    @Override
+    protected RepartirStrategy getRepartirStrategy() {
+        return (mazo, commonCards, playerCards) -> {
+            player1 = playerCards.get(0);
+            player2 = playerCards.get(1);
+
+            player1.setCards(Card.of(Suit.Clubs, Rank.KING), Card.of(Suit.Spades, Rank.TWO));
+            mazo.removeCards(player1.getCards());
+
+            player2.setCards(Card.of(Suit.Hearts, Rank.NINE), Card.of(Suit.Diamonds, Rank.EIGHT));
+            mazo.removeCards(player2.getCards());
+
+            commonCards.setFlop(Card.of(Suit.Clubs, Rank.ACE), Card.of(Suit.Spades, Rank.FOUR), Card.of(Suit.Clubs, Rank.SEVEN));
+            commonCards.setTurn(Card.of(Suit.Spades, Rank.NINE));
+            mazo.removeCards(commonCards.getCards());
+
+            commonCards.setRiver(mazo.getRandomCard());
+        };
+    }
+
+    @Override
+    protected List<EventListener> setupEventListeners() {
+        return List.of(new EventListener() {
+            private int player1Counter = 0;
+            private int player2Counter = 0;
+
+            private int tie = 0;
+
+            @Override
+            public void raiseEvent(HoldemEvents event, Holdem holdem) {
+                if (event.equals(HoldemEvents.FINISHED)) {
+                    if (holdem.getGanadores().size() == 1) {
+                        if (holdem.getGanadores().contains(player1)) {
+                            player1Counter++;
+                        }
+                        if (holdem.getGanadores().contains(player2)) {
+                            player2Counter++;
+                        }
+                    } else if (holdem.getGanadores().size() == 2) {
+                        tie++;
+                    }
+                }
+            }
+
+            @Override
+            public void printStatics() {
+                System.out.println("Player1 = \t\t\t" + String.format("%3.2f%%", 100f * (float) player1Counter / (float) SIMULATIONS));
+                System.out.println("Player2 = \t\t\t" + String.format("%3.2f%%", 100f * (float) player2Counter / (float) SIMULATIONS));
+                System.out.println("Tie = \t\t\t" + String.format("%3.2f%%", 100f * (float) tie / (float) SIMULATIONS));
+            }
+        });
+    }
+}
