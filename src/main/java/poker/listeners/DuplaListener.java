7dbe40570db6469846060a957ea8bc0d5494fcb0
Mauricio Coria <coriamauricio@gmail.com>
2022-12-29 19:10:26 -0300

    Deleting TipoCombinacion enum

--- src/main/java/poker/listeners/DuplaListener.java
+++ src/main/java/poker/listeners/DuplaListener.java
@@ -4,7 +4,6 @@ import poker.Card;
 import poker.Holdem;
 import poker.Player;
 import poker.simulations.Dupla;
-import poker.simulations.TipoCombinacion;
 
 import java.util.*;
 
@@ -17,11 +16,11 @@ public class DuplaListener implements HoldemStatics {
     public void catchEvent(HoldemEvents event, Holdem holdem) {
         if (event.equals(HoldemEvents.FINISHED)) {
             Set<Dupla> dupleSet = new HashSet<>();
-            for (Player player: holdem.getPlayers() ) {
+            for (Player player : holdem.getPlayers()) {
                 Card[] cards = player.getCards().toArray(new Card[2]);
                 Dupla dupla = new Dupla(cards[0], cards[1]);
                 // Si el juego es ganador evitamos contarlo mas de una vez
-                if(!dupleSet.contains(dupla)) {
+                if (!dupleSet.contains(dupla)) {
                     contadoresJuego.compute(dupla, (k, v) -> v == null ? 1 : v + 1);
                     if (holdem.getGanadores().contains(player)) {
                         contadoresJuegoGanador.compute(dupla, (k, v) -> v == null ? 1 : v + 1);
@@ -34,11 +33,11 @@ public class DuplaListener implements HoldemStatics {
 
     @Override
     public void printStatics() {
-        contadoresJuegoGanador.entrySet().stream().sorted( Comparator.comparingDouble(entry -> (double) entry.getValue() / contadoresJuego.get(entry.getKey())) ) .forEach( entry ->{
+        contadoresJuegoGanador.entrySet().stream().sorted(Comparator.comparingDouble(entry -> (double) entry.getValue() / contadoresJuego.get(entry.getKey()))).forEach(entry -> {
             Dupla dupla = entry.getKey();
             int contadorGanador = entry.getValue();
             int contadorJuego = contadoresJuego.get(dupla);
-            System.out.printf("Probabilidad de ganar con %s  = \t\t\t %3.2f%% \n",  dupla.toString(), 100f * (float) contadorGanador / (float) contadorJuego);
+            System.out.printf("Probabilidad de ganar con %s  = \t\t\t %3.2f%% \n", dupla.toString(), 100f * (float) contadorGanador / (float) contadorJuego);
         });
     }
 
--- src/test/java/poker/CombinacionTest.java
+++ src/test/java/poker/CombinacionTest.java
@@ -2,7 +2,6 @@ package poker;
 
 import org.junit.Test;
 import poker.simulations.Dupla;
-import poker.simulations.TipoCombinacion;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +11,6 @@ public class CombinacionTest {
 
     @Test
     public void combinar(){
-        //Map<TipoCombinacion, Integer> combinatoria = new HashMap<>();
         Map<Dupla, Integer> duplaCounters = new HashMap<>();
         Card[] cardArray = Card.values();
         int counter = 0;
@@ -20,9 +18,6 @@ public class CombinacionTest {
             Card card1 = cardArray[i];
             for (int j = i + 1; j < 52; j++) {
                 Card card2 = cardArray[j];
-
-                //TipoCombinacion combinacion = TipoCombinacion.loadTipo(Set.of(card1, card2));
-                // combinatoria.compute(combinacion, (k, v) -> v == null ? 1 : v + 1);
 
                 Dupla dupla = new Dupla(card1, card2);
                 duplaCounters.compute(dupla, (k, v) -> v == null ? 1 : v + 1);
@@ -32,15 +27,6 @@ public class CombinacionTest {
         }
         System.out.printf("Combinaciones posibles %d \n", counter);
 
-        /*
-        for (TipoCombinacion combinacion :
-                TipoCombinacion.values()) {
-            if (combinatoria.containsKey(combinacion)) {
-                int contador = combinatoria.get(combinacion);
-                System.out.printf("Combinacion '%s' \t\t %d \t %3.2f%% \n", combinacion.getDescription(), contador, 100f * (float) contador / (float) counter);
-            }
-        }
-         */
 
         for (Map.Entry<Dupla, Integer> duplaCounter:
                 duplaCounters.entrySet()) {
